4e92c360052391ac94e9e2efa8d4d6db90fe18fb
tikkoro <tikkoro@gmail.com>
2023-01-19 16:01:44 +0900

    feat: add ssg and ssr sample page

---
+++ src/pages/ssr_sample.tsx
@@ -0,0 +1,34 @@
+import { Center } from '@chakra-ui/react';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import { Product } from '@/features/top/types/product';
+
+type Props = {
+    posts: Product[];
+};
+
+const SSR = ({ posts }: Props): JSX.Element => {
+    return (
+        <>
+            <Center>ssr</Center>
+            {posts.map((post) => (
+                <Center key={post.id}>{post.title}</Center>
+            ))}
+        </>
+    );
+};
+
+export default SSR;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const API_URL_ROOT = 'https://fakestoreapi.com/products';
+    const res = await axios.get(API_URL_ROOT);
+    const posts = res.data as Product[];
+    console.log(posts);
+
+    return {
+        props: {
+            posts,
+        },
+    };
+};
